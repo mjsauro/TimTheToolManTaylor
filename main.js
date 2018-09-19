310fa4090ae0bc51efb19424b6f82f1ab0192c6e
mjsauro <mjsauro@gmail.com>
2018-09-19 18:25:34 -0500

    fixing pathing

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     
     var obj = document.createElement("audio");
-    obj.src = "/Tim Allen aargh2.wav";
+    obj.src = "TimAllenaargh2.wav";
     obj.volume = 0.1;
     obj.autoPlay = false; 
     obj.preLoad = true;
